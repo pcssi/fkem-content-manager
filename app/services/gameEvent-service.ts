6d2cbec6e3bebf5f3cae07d73a0fe790c6cb9d9f
Chris Stevens <munkeyxis@gmail.com>
2016-01-10 20:25:59 -0500

    add gameEvent service

---
+++ app/services/gameEvent-service.ts
@@ -0,0 +1,26 @@
+export class GameEvent {
+	constructor(
+		public name: string,
+		public description: string
+	) {}
+}
+
+export class GameEventService {
+	getGameEvent(): Array<GameEvent> {
+		return gameEvents.map(e => new GameEvent(e.name, e.description));
+	}
+}
+
+var gameEvents = [
+	{
+		name: 'The First Event',
+		description: `
+			After you have read the description choose from one of the options below
+		`
+	}, {
+		name: 'The Second Event',
+		description: `
+			You should know what to do now
+		`
+	}
+];
