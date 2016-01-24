9c88876f70de6438114526e98f769396ba81d3f8
Chris Stevens <munkeyxis@gmail.com>
2016-01-23 23:43:07 -0500

    remove services from boostrapper

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,10 @@
 import {bootstrap} from 'angular2/platform/browser';
 import 'rxjs/add/operator/map';
 import {ROUTER_PROVIDERS} from 'angular2/router';
-import {HTTP_PROVIDERS, Http} from 'angular2/http';
-import {GameEventsService} from './services/game-events.service';
+import {HTTP_PROVIDERS} from 'angular2/http';
 import ApplicationComponent from './components/application/application';
 
 bootstrap(ApplicationComponent, [
 	ROUTER_PROVIDERS,
-	HTTP_PROVIDERS,
-	GameEventsService
+	HTTP_PROVIDERS
 ]);
